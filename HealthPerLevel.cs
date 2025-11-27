2514338bfc184a9d68c51af81275659033588768
Andrzej Frysiak <fryciarz7@gmail.com>
2025-11-27 12:09:52 +0100

    adds ModMetadata

--- HealthPerLevel.cs
+++ HealthPerLevel.cs
@@ -1,6 +1,22 @@
-﻿namespace HealthPerLevel_cs
+﻿using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace HealthPerLevel_cs;
+public record ModMetadata : AbstractModMetadata
 {
-    public class HealthPerLevel
-    {
-    }
+    public override string ModGuid { get; init; } = "com.fryciarz7.spt.hpl";
+    public override string Name { get; init; } = "Health Per Level";
+    public override string Author { get; init; } = "fryciarz7";
+    public override List<string>? Contributors { get; init; }
+    public override SemanticVersioning.Version Version { get; init; } = new("0c.1.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; }
+    public override string? License { get; init; } = "Creative Commons BY-NC-SA 3.0";
+}
+
+public class HealthPerLevel
+{
 }
