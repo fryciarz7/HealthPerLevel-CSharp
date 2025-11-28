4c972ede3b09be1ecdf10e312eecd0e4c82c9ddf
Andrzej Frysiak <fryciarz7@gmail.com>
2025-11-29 00:34:52 +0100

    implements interfaces for config

---
+++ Interfaces/ICharacter.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthPerLevel_cs.Interfaces
+{
+    public interface ICharacter
+    {
+        public int levels_per_increment { get; set; }
+        public bool level_cap { get; set; }
+        public int level_cap_value { get; set; }
+    }
+}
