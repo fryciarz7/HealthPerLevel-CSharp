139a559bde8601cfb7bcbd5c001cc3479574db98
Andrzej Frysiak <fryciarz7@gmail.com>
2026-01-15 11:20:42 +0100

    fixes typo

--- HealthPerLevel.cs
+++ HealthPerLevel.cs
@@ -217,7 +217,7 @@ namespace HealthPerLevel_cs
                 {
                     SptProfile? profile = kvp.Value;
                     //_logger.Info($"{LogPrefix}Modifying health for profile: {profile?.ProfileInfo?.Username} with experience: {profile?.CharacterData?.PmcData?.Info?.Experience}");
-                    _logger.Info($"{LogPrefix}Modifying health for profile: {profile?.ProfileInfo?.Username})";
+                    _logger.Info($"{LogPrefix}Modifying health for profile: {profile?.ProfileInfo?.Username}");
                     if (profile?.CharacterData?.PmcData != null)
                     {
                         CalculateCharacterData(profile.CharacterData.PmcData, _config.PMC);
