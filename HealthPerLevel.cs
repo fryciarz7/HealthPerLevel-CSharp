e7a2cb605d39836a9f9eed60cceaa520eefe789b
Andrzej Frysiak <fryciarz7@gmail.com>
2026-01-15 11:20:25 +0100

    adds log with profile name

--- HealthPerLevel.cs
+++ HealthPerLevel.cs
@@ -216,7 +216,8 @@ namespace HealthPerLevel_cs
                 try
                 {
                     SptProfile? profile = kvp.Value;
-                    _logger.Info($"{LogPrefix}Modifying health for profile: {profile.ProfileInfo.Username}");
+                    //_logger.Info($"{LogPrefix}Modifying health for profile: {profile?.ProfileInfo?.Username} with experience: {profile?.CharacterData?.PmcData?.Info?.Experience}");
+                    _logger.Info($"{LogPrefix}Modifying health for profile: {profile?.ProfileInfo?.Username})";
                     if (profile?.CharacterData?.PmcData != null)
                     {
                         CalculateCharacterData(profile.CharacterData.PmcData, _config.PMC);
