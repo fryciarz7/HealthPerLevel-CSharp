695202bd91d266aa625a5893eb32e62323574040
Andrzej Frysiak <fryciarz7@gmail.com>
2026-01-18 12:28:09 +0100

    adjust restore default levels

--- HealthPerLevel.cs
+++ HealthPerLevel.cs
@@ -246,8 +246,13 @@ namespace HealthPerLevel_cs
         private void CalculateCharacterData<T, E, G>(PmcData character, ICharacter<T, E, G> charType, bool restoreDefault)
         {
             ValidateProfile(character, charType);
-            double? accLv = restoreDefault ? 1 : CheckLevelCap(character, charType);
-            double healthSkill = restoreDefault ? 1 : GetHealthLevel(character, charType);
+            double? accLv = restoreDefault ? 0 : CheckLevelCap(character, charType);
+            double healthSkill = restoreDefault ? 0 : GetHealthLevel(character, charType);
+            if (_config.debug)
+            {
+                _logger.Info($"{LogPrefix}accLv: {accLv}");
+                _logger.Info($"{LogPrefix}healthSkill: {healthSkill}");
+            }
             foreach (var (bodyPartName, bodyPart) in character.Health.BodyParts)
             {
                 if (bodyPart != null && bodyPart.Health != null)
