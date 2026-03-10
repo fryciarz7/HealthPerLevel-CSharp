63f1edd24045e720208c0461984c1e6bba913287
Andrzej Frysiak <fryciarz7@gmail.com>
2026-03-10 19:57:56 +0100

    adds metabolism per skill in ICharacter

--- Interfaces/ICharacter.cs
+++ Interfaces/ICharacter.cs
@@ -17,5 +17,6 @@
         public bool metabolism_skill_cap { get; set; }
         public int metabolism_skill_cap_value { get; set; }
         public IMetabolism metabolism { get; set; }
+        public IMetabolism metabolism_per_skill { get; set; }
     }
 }
